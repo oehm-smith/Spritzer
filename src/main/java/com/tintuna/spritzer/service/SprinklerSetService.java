980302f7750bfed65508e09a54e5fd92f40f9bca
Brooke Smith <brooke@tintuna.com>
2012-10-31 10:28:39 +1100

    1. Added license header to each of my source files. 2. Activated checkstyle formatting which updated the .pom.

--- src/main/java/com/tintuna/spritzer/service/SprinklerSetService.java
+++ src/main/java/com/tintuna/spritzer/service/SprinklerSetService.java
@@ -1,11 +1,21 @@
-/* 
- * Copyright (c) Tintuna Software (2012).
- * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
+/**
+* This file is part of Spritzer.
+ 
+Spritzer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+Spritzer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Spritzer.  If not, see http://www.gnu.org/licenses/.
+ 
+* Copyright 2012 Brooke Smith, tintuna.com.
+**/
 package com.tintuna.spritzer.service;
 
 import com.tintuna.spritzer.crud.CrudService;
